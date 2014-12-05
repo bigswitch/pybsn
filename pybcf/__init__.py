32b46082b433f379a4d52d28e06251126baf8ba5
Rich Lane <rlane@bigswitch.com>
2014-12-04 21:49:07 -0800

    use attrdict when deserializing JSON objects
    This gives us convenient attribute syntax.

--- pybcf/__init__.py
+++ pybcf/__init__.py
@@ -1,5 +1,17 @@
-import requests, json
+import requests, json, attrdict
 PREFIX = "/api/v1/data/"
+
+class BCFJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, attrdict.AttrDict):
+            return obj._mapping
+        return json.JSONEncoder.default(self, obj)
+
+def to_json(data):
+    return json.dumps(data, cls=BCFJSONEncoder)
+
+def from_json(text):
+    return json.loads(text, object_hook=attrdict.AttrDict)
 
 class Node(object):
     def __init__(self, path, session):
@@ -15,13 +27,13 @@ class Node(object):
         return self.session.request(method, url, data=data, params=params)
 
     def get(self, params=None):
-        return self.request("GET", params=params).json()[0]
+        return from_json(self.request("GET", params=params).text)[0]
 
     def post(self, data):
-        self.request("POST", data=json.dumps(data))
+        self.request("POST", data=to_json(data))
 
     def patch(self, data):
-        self.request("PATCH", data=json.dumps(data))
+        self.request("PATCH", data=to_json(data))
 
     def __call__(self):
         return self.get()
