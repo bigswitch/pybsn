65457f01ca5d1f786c8b65c7bf187864d96dc526
Rich Lane <rlane@bigswitch.com>
2014-12-04 22:10:46 -0800

    prefix Node instance variables with underscores
    To reduce the chance of name collisions.

--- pybcf/__init__.py
+++ pybcf/__init__.py
@@ -15,25 +15,25 @@ def from_json(text):
 
 class Node(object):
     def __init__(self, path, session):
-        self.path = path
-        self.session = session
+        self._path = path
+        self._session = session
 
     def __getattr__(self, name):
         name = name.replace("_", "-")
-        return Node(self.path + "/" + name, self.session)
+        return Node(self._path + "/" + name, self._session)
 
-    def request(self, method, data=None, params=None):
-        url = self.session.url + PREFIX + self.path
-        return self.session.request(method, url, data=data, params=params)
+    def _request(self, method, data=None, params=None):
+        url = self._session.url + PREFIX + self._path
+        return self._session.request(method, url, data=data, params=params)
 
     def get(self, params=None):
-        return from_json(self.request("GET", params=params).text)[0]
+        return from_json(self._request("GET", params=params).text)[0]
 
     def post(self, data):
-        self.request("POST", data=to_json(data))
+        self._request("POST", data=to_json(data))
 
     def patch(self, data):
-        self.request("PATCH", data=to_json(data))
+        self._request("PATCH", data=to_json(data))
 
     def __call__(self):
         return self.get()
