7793839db031f71992d896b3448a957eddfc4b0d
Rich Lane <rlane@bigswitch.com>
2014-12-06 16:35:17 -0800

    examples: add schema.py
    Usage:
    
        PYTHONPATH=. python examples/schema.py controller.applications.bcf.tenant.segment
    
    Outputs:
    
        controller/applications/bcf/tenant/segment (list)
          endpoint (list)
            attachment-point
              interface : string
              switch : string
              vlan : integer
              port-group : string
            description : string
            origination : string
            mac : string
            ip-address (list)
              ip-address : union
            name : string
          name : string
          member-vlan : integer
          port-group-membership-rule (list)
            origination : string
            vlan : integer
            description : string
            port-group : string
          origination : string
          switch-port-membership-rule (list)
            interface : string
            switch : string
            vlan : integer
            origination : string
            description : string
          internal-vlan : integer
          description : string

---
+++ examples/schema.py
@@ -0,0 +1,37 @@
+import pybcf
+import argparse
+
+parser = argparse.ArgumentParser(description='Add a static endpoint')
+
+parser.add_argument('path', type=str, default='controller', nargs='?')
+parser.add_argument('--host', '-H', type=str, default="127.0.0.1", help="Controller IP/Hostname to connect to")
+parser.add_argument('--user', '-u', type=str, default="admin", help="Username")
+parser.add_argument('--password', '-p', type=str, default="adminadmin", help="Password")
+
+args = parser.parse_args()
+
+bcf = pybcf.BCF("http://%s:8080" % args.host, args.user, args.password)
+
+def traverse(node, depth=0, name="root"):
+    def output(*s):
+        print " " * (depth * 2) + ' '.join(s)
+    if node['nodeType'] == 'CONTAINER' or node['nodeType'] == 'LIST_ELEMENT':
+        if node['nodeType'] == 'LIST_ELEMENT':
+            output(name, "(list)")
+        else:
+            output(name)
+        for child_name in node['childNodes']:
+            child = node['childNodes'][child_name]
+            traverse(child, depth+1, child_name)
+    elif node['nodeType'] == 'LIST':
+        traverse(node['listElementSchemaNode'], depth, name)
+    elif node['nodeType'] == 'LEAF':
+        if True:
+            output(name, ":", node['leafType'].lower())
+    elif node['nodeType'] == 'LEAF_LIST':
+        output(name, "(leaf list)")
+    else:
+        assert False, "unknown node type %s" % node['nodeType']
+
+path = args.path.replace('.', '/')
+traverse(bcf.schema(path), name=path)
