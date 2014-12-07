b12cf10c688723fd6679751cbc543c0d9cfea04a
Rich Lane <rlane@bigswitch.com>
2014-12-07 13:09:01 -0800

    examples/schema.py: add --max-depth option

--- examples/schema.py
+++ examples/schema.py
@@ -8,6 +8,8 @@ parser.add_argument('--host', '-H', type=str, default="127.0.0.1", help="Control
 parser.add_argument('--user', '-u', type=str, default="admin", help="Username")
 parser.add_argument('--password', '-p', type=str, default="adminadmin", help="Password")
 
+parser.add_argument("--max-depth", "-d", type=int, help="Maximum recursion depth")
+
 args = parser.parse_args()
 
 bcf = pybcf.BCF("http://%s:8080" % args.host, args.user, args.password)
@@ -15,6 +17,8 @@ bcf = pybcf.BCF("http://%s:8080" % args.host, args.user, args.password)
 def traverse(node, depth=0, name="root"):
     def output(*s):
         print " " * (depth * 2) + ' '.join(s)
+    if args.max_depth is not None and depth > args.max_depth:
+        return
     if node['nodeType'] == 'CONTAINER' or node['nodeType'] == 'LIST_ELEMENT':
         if node['nodeType'] == 'LIST_ELEMENT':
             output(name, "(list)")
