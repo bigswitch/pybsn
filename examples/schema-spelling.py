5a28f794fb2fd0fff96ab8b3d057b42596346009
Rich Lane <rlane@bigswitch.com>
2014-12-07 23:37:00 -0800

    add examples/schema-spelling.py
    This program uses the enchant library to check for spelling errors in the
    descriptions in the schema.

---
+++ examples/schema-spelling.py
@@ -0,0 +1,47 @@
+import pybcf
+import argparse
+from enchant.checker import SpellChecker
+
+parser = argparse.ArgumentParser(description='Check spelling of schema descriptions')
+
+parser.add_argument('path', type=str, default='controller', nargs='?')
+parser.add_argument('--host', '-H', type=str, default="127.0.0.1", help="Controller IP/Hostname to connect to")
+parser.add_argument('--user', '-u', type=str, default="admin", help="Username")
+parser.add_argument('--password', '-p', type=str, default="adminadmin", help="Password")
+
+args = parser.parse_args()
+
+bcf = pybcf.BCF("http://%s:8080" % args.host, args.user, args.password)
+
+names = []
+descriptions = []
+
+def traverse(node):
+    if 'name' in node:
+        names.append(node['name'])
+
+    if 'description' in node:
+        descriptions.append(node['description'])
+
+    if node['nodeType'] == 'CONTAINER' or node['nodeType'] == 'LIST_ELEMENT':
+        for child_name in node['childNodes']:
+            traverse(node['childNodes'][child_name])
+    elif node['nodeType'] == 'LIST':
+        traverse(node['listElementSchemaNode'])
+
+traverse(bcf.root.schema())
+
+chkr = SpellChecker("en_US")
+chkr.set_text(' '.join(names).lower())
+name_errors = set()
+for err in chkr:
+    name_errors.add(chkr.word)
+
+description_errors = set()
+chkr.set_text(' '.join(descriptions).lower())
+for err in chkr:
+    if chkr.word not in name_errors:
+        description_errors.add(chkr.word)
+
+for word in sorted(description_errors):
+    print word
