d3e27a92332b3ee2fec7798b8aa2597e5bb2651d
Sovietaced <Sovietaced@gmail.com>
2014-12-17 23:56:12 -0800

    mock http server skeleton

--- pybsn/test/test.py
+++ pybsn/test/test.py
@@ -1,2 +1,42 @@
-def test_b():
-    assert 'b' == 'b'
+#!/usr/bin/env python
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+import threading
+from nose.tools import with_setup
+
+class BSNRestAPI(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+
+    def do_HEAD(self):
+        self._set_headers()
+        
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        self._set_headers()
+        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
+        
+def run(server_class=HTTPServer, handler_class=BSNRestAPI, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print 'Starting httpd...'
+    httpd.serve_forever()
+
+def setUp():
+    task = run
+    thread = threading.Thread(target=task)
+    thread.daemon = True
+    thread.start()
+
+def tearDown():
+	pass
+
+@with_setup(setUp, tearDown)
+def test_base():
+	print "testing"
