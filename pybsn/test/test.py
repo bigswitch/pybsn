c83ae4d99fb2de5c359b6c07e92b832398afabe2
Sovietaced <Sovietaced@gmail.com>
2015-01-13 01:26:14 -0800

    use a more complex schema and handle more leaf types

--- pybsn/test/test.py
+++ pybsn/test/test.py
@@ -1,47 +1,67 @@
 #!/usr/bin/env python
+# coding: interpy
 import unittest
 from nose.tools import with_setup
 import responses
 import requests
 import json
+import random
 
 PREFIX = "/api/v1/data/"
 
 @responses.activate
 
+#TODO: Find a sexier way to mock up response types in accordanance with validations
 def mock_leaf_response(leaf_node, path):
 
     type_schema_node = leaf_node['typeSchemaNode']
     node_type = type_schema_node['leafType']
 
+    body = "{\"#{leaf_node['name']}\":"
     if node_type == 'STRING':
-        body = "string"
+        body += '"string"}'
+    elif node_type == 'BOOLEAN':
+        body += 'false}'
+    elif node_type == 'INTEGER':
+        body += '1}'
+    elif node_type == 'ENUMERATION':
+        # Will there ever be more than one type validator?
+        enums = type_schema_node['typeValidator'][0]['names'].keys()
+        enum = random.choice(enums)
+        body += "\"#{enum}\"}"
+    else:
+        print "Missing node_type #{node_type}"
 
+    print body
     responses.add(responses.GET, PREFIX + path,
               body=body, status=200,
               content_type='application/json')
 
 def traverse(node, path="controller"):
-    if node['nodeType'] == 'CONTAINER' or node['nodeType'] == 'LIST_ELEMENT':
-        for child in node['childNodes']:
-            traverse(child)
-    elif node['nodeType'] == 'LIST':
-        traverse(node['listElementSchemaNode'])
-    elif node['nodeType'] == 'LEAF':
-        path += "/" + node['name']
-        mock_leaf_response(node, path)
-    elif node['nodeType'] == 'LEAF_LIST':
-        pass
-        # do stuff
+
+    if type(node) is dict:
+        if node['nodeType'] == 'CONTAINER' or node['nodeType'] == 'LIST_ELEMENT':
+            for child in node['childNodes']:
+                traverse(node['childNodes'][child])
+        elif node['nodeType'] == 'LIST':
+            traverse(node['listElementSchemaNode'])
+        elif node['nodeType'] == 'LEAF':
+            path += "/" + node['name']
+            mock_leaf_response(node, path)
+        elif node['nodeType'] == 'LEAF_LIST':
+            pass
+            # do stuff
+        else:
+            assert False, "unknown node type %s" % node['nodeType']
     else:
-        assert False, "unknown node type %s" % node['nodeType']
+        print node
 
 def load_schema():
     json_data=open('schema')
-    node = json.load(json_data)
+    root_node = json.load(json_data)
     json_data.close()
 
-    traverse(node)
+    traverse(root_node)
 
 
 class BigDBClientTest(unittest.TestCase):
