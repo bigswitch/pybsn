c97bc400f92ce332d5b7337e477bb371ea1d7270
Sovietaced <Sovietaced@gmail.com>
2015-05-20 16:18:25 -0700

    use environment variables

--- pybsn/test/test_switch.py
+++ pybsn/test/test_switch.py
@@ -1,3 +1,4 @@
+import os
 from betamax import Betamax
 from requests import Session
 from unittest import TestCase
@@ -7,8 +8,19 @@ with Betamax.configure() as config:
     config.cassette_library_dir = 'fixtures/cassettes/switch'
 
 def test_get_switches():
-	api = Api("192.168.142.213", "admin", "taco")
+	api = Api(os.environ['PYBSN_HOST'], os.environ['PYBSN_USER'], os.environ['PYBSN_PASS'])
 	with Betamax(api.client.session).use_cassette('get_switches'):
+		switches = api.get_switches()
+
+		assert len(switches) == 14
+
+		for sw in switches:
+			assert sw.name == None
+			assert sw.dpid != None
+
+def test_disconnect_switch():
+	api = Api(os.environ['PYBSN_HOST'], os.environ['PYBSN_USER'], os.environ['PYBSN_PASS'])
+	with Betamax(api.client.session).use_cassette('disconnect_switch'):
 		switches = api.get_switches()
 
 		assert len(switches) == 14
