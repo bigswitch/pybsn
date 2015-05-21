00c61fff5eaf8500dc24f72b035d3cb5743bc40e
Sovietaced <Sovietaced@gmail.com>
2015-05-20 18:32:37 -0700

    begin docs for api class

--- pybsn/api.py
+++ pybsn/api.py
@@ -1,7 +1,16 @@
 from __init__ import connect
 from switch import Switch
 
-class Api(object): 
+class Api(object):
+    """A python interface into the Big Switch Networks Big Cloud Fabric API
+
+    Example usage:
+  
+      To create an instance of the pybsn.Api class:
+  
+        >>> import pybsn
+        >>> api = pybsn.Api(<controller_ip>, <user>, <password>)
+    """ 
 
     def __init__(self,
                 host,
