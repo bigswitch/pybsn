a73a5b10484f13e1c0cfe78ed2acd0d8be12d682
Sovietaced <Sovietaced@gmail.com>
2015-05-21 17:30:46 -0700

    add test for interfaces

--- pybsn/test/test_switch.py
+++ pybsn/test/test_switch.py
@@ -41,3 +41,16 @@ def test_disconnect_switch():
 
 		for sw in switches:
 			sw.disconnect()
+
+def test_get_interfaces():
+	api = Api(os.environ['PYBSN_HOST'], os.environ['PYBSN_USER'], os.environ['PYBSN_PASS'])
+	with Betamax(api.client.session).use_cassette('get_interfaces'):
+		switches = api.get_switches(filter={"dpid":"00:00:00:00:00:01:00:01"})
+
+		assert len(switches) == 1
+
+		sw = switches[0]
+
+		interfaces = sw.get_interfaces()
+
+		assert len(interfaces) == 9
