f949f6caffa410ee8feb9a726618ba26e6d5a3bf
Andreas Wundsam <andreas.wundsam@arista.com>
2025-03-24 16:50:35 -0700

    BUG1117675 bump version to 0.4.2 in preparation for release

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='pybsn',
-    version='0.4.1',
-    description="pybsn is a python interface to Big Switch Networks' products",
-    url='https://github.com/floodlight/pybsn',
+    version='0.4.2',
+    description="pybsn is a python interface to Arista Networks BigDB/Atlas based products",
+    url='https://github.com/bigswitch/pybsn',
     author='Andreas Wundsam, Andre Kutzleb, Jason Parraga, Rich Lane',
-    author_email='Andreas.Wundsam@arista.com, Andre.Kutzleb@arista.com',
+    author_email='Andreas.Wundsam@arista.com',
     license='ECLIPSE',
     packages=['pybsn'],
     zip_safe=False,
